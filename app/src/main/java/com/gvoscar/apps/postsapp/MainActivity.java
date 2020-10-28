831d1401e8c9e32cd365a4d3d6ceb8cd4e0ed4ab
Oscar Giraldo <oscargiraldo000@gmail.com>
2020-10-28 12:50:45 -0500

    Inicio de proyecto

---
+++ app/src/main/java/com/gvoscar/apps/postsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gvoscar.apps.postsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
